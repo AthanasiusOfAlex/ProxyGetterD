706cd3535879b4b8413e5b1721941507206a4121
AthanasiusOfAlex <lmelahn@Louiss-MacBook-Pro.local>
2016-10-19 16:38:54 +0200

    Added to-do list

--- ProxyGetterD/main.d
+++ ProxyGetterD/main.d
@@ -4,6 +4,11 @@ import std.stdio;
 import std.traits;
 import ProxyGetter.ProxyGetter;
 
+// TODO:
+//   (1) see if NSDictionary can be implemented as an array of NSObject,
+//       and then use introspection to get the correct datatype
+//   (2) Use Cocoa's enums to get the proxy settings:
+//       https://developer.apple.com/reference/systemconfiguration/1517088-scdynamicstorecopyproxies
 
 void main(string[] args)
 {
